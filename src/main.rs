cec0e1af99c4c579453808ed056f727a1f252340
Roudey <simonski05@gmail.com>
2022-05-09 22:54:51 +0200

    Shortened Code

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,31 @@
 use std::{fs::{read_to_string}, env::{args}};
 
-#[derive(PartialEq)]
-#[derive(Debug)]
-enum Instructions {
-    PointerToRight,
-    PointerToLeft,
-    Increment,
-    Decrement,
-    Output,
-    Input,
-    LoopStart,
-    LoopEnd,
-}
-
 #[allow(arithmetic_overflow)]
 fn main() {
     let mut memory = vec![0u8; 30000];
     let mut pointer = 0;
 
-    let mut instructions: Vec<Instructions> = vec![];
-    
-    for i in read_file().chars() {
-        match i {
-            '>' => instructions.push(Instructions::PointerToRight),
-            '<' => instructions.push(Instructions::PointerToLeft),
-            '+' => instructions.push(Instructions::Increment),
-            '-' => instructions.push(Instructions::Decrement),
-            '.' => instructions.push(Instructions::Output),
-            ',' => instructions.push(Instructions::Input),
-            '[' => instructions.push(Instructions::LoopStart),
-            ']' => instructions.push(Instructions::LoopEnd),
+    for instruction in read_file().chars() {
+        match instruction {
+            '>' => pointer += 1,
+            '<' => pointer -= 1,
+            '+' => memory[pointer] += 1,
+            '-' => memory[pointer] -= 1,
+            '.' => print!("{}", memory[pointer] as char),
+            ',' => memory[pointer] = get_input() as u8,
+            '[' => {
+                
+            }
+            ']' => {
+                
+            }
             _ => {}
-        }
-    }
-
-    for i in &instructions {
-        match i {
-            Instructions::PointerToRight => pointer += 1,
-            Instructions::PointerToLeft => pointer -= 1,
-            Instructions::Increment => memory[pointer] += 1,
-            Instructions::Decrement => memory[pointer] -= 1,
-            Instructions::Output => print!("{}", memory[pointer] as char),
-            Instructions::Input => memory[pointer] = get_input() as u8,
-            Instructions::LoopStart => {
-                
-            }
-            Instructions::LoopEnd => {
-                
-            }
         }
     }
 }
 
 fn read_file() -> String {
-    //Get file path
     let path = args().nth(1).expect("No file path provided");
-    //Read file to string
     let contents = read_to_string(path).expect("Unable to read file");
     contents
 }
